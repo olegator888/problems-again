53dc649ec008a133ecfc7dfe71cfbd6351a6224f
olegator888 <139753116+olegator888@users.noreply.github.com>
2024-07-26 18:26:52 +0300

    127-Word-Ladder

---
+++ graphs/127-Word-Ladder/main.py
@@ -0,0 +1,31 @@
+from collections import defaultdict, deque
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList) -> int:
+        if endWord not in wordList:
+            return 0
+
+        nei = defaultdict(list)
+        wordList.append(beginWord)
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j+1:]
+                nei[pattern].append(word)
+
+        q = deque([beginWord])        
+        visit = set([beginWord])
+        res = 1
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for j in range(len(word)):
+                    pattern = word[:j] + "*" + word[j+1:]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res += 1
+
+        return 0    
