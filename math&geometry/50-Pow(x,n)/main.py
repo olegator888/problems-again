453f48766cc27c7130e8be642886d719f3099096
olegator888 <139753116+olegator888@users.noreply.github.com>
2024-09-27 18:13:57 +0300

    50-Pow(x,n)

--- math&geometry/50-Pow(x,n)/main.py
+++ math&geometry/50-Pow(x,n)/main.py
@@ -1,15 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         power = abs(n)
-        res = 1
 
-        while power > 0:
-            res *= x
-            power -= 1
-
+        def helper(power):
+            if power == 0:
+                return 1
+            if power == 1:
+                return x
+            if power == 2:
+                return x * x        
+            half = helper(power // 2)
+            return half * half * (x if power % 2 == 1 else 1)
+        
+        res = helper(power)  
+            
         if n < 0:
             return 1 / res
-        return res    
-    
-    # пока что tle 
-    # возможно, стоит попробовать прыгать по 2
+        return res   
